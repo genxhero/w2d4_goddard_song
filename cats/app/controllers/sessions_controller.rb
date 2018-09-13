97a3bdf76bac33ac3c35e58719ccfeb909caedda
aarongoddard <aaronbear@gmail.com>
2018-09-12 17:05:06 -0700

    Minor changes

--- cats/app/controllers/sessions_controller.rb
+++ cats/app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   before_action :require_login, only: [:destroy]
   before_action :require_logout, only: [:new, :create]
 
+
   def new
     @user = User.new
     render :new
